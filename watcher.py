005014d217de8b38f1531a4606dd5c508758df6a
angmusae2015 <angmusae2015@gmail.com>
2023-12-07 14:48:33 +0000

    BACKUP

--- watcher.py
+++ watcher.py
@@ -53,12 +53,30 @@ class Alarm:
         self.condition = condition
 
 
+class Candle:
+    def __init__(self, exchange_id: int, symbol: str, timestamp: int, interval: str, open: float, highest: float, lowest: float, closing: float, volume: float):
+        self.exchange_id = exchange_id
+        self.symbol = symbol
+        self.timestamp = timestamp
+        self.interval = interval
+        self.open = open
+        self.highest = highest
+        self.lowest = lowest
+        self.closing = closing
+        self.volume = volume
+
+
 UPBIT_ID = 1
 BINANCE_ID = 2
+INTERVAL_SECOND_DICT = {
+    '1s': 1,
+    '1m': 60,
+    '1h': 3600,
+    '1d': 86400
+}
 
 class Watcher:
-    enabled_upbit_alarm_list = []
-    enabled_binance_alarm_list = []
+    enabled_alarm_dict = {}
 
     # 한 알람 사이클을 돌면서 확인한 종목 주가 정보를 캐싱한 딕셔너리
     # 같은 종목의 다른 알림이 있을 경우 효율적으로 주가 정보를 확인하기 위해 캐싱
@@ -68,7 +86,8 @@ class Watcher:
     {
         symbol: {
             order_book: {},
-            tick: {}
+            tick: {},
+            ohlcv: []
         },
         ...
     }
@@ -78,13 +97,14 @@ class Watcher:
         BINANCE_ID: {}
     }
 
+    candle_cache: List[Candle] = []
+
     # 전 사이클의 호가 정보를 저장하는 딕셔너리
     # 발견한 고래가 전과 중복되는 고래인지 확인하기 위함
     order_book_before = {
         UPBIT_ID: {},
         BINANCE_ID: {}
     }
-
 
     CYCLE_MILLISECOND = 5000
 
@@ -99,8 +119,7 @@ class Watcher:
         self.binance.enableRateLimit = True
 
 
-    # 데이터베이스에서 활성화된 알람을 조회해 각 거래소 별로 enabled_upbit_alarm_list/enabled_binance_alarm_list 요소에 Alarm 객체의 리스트로 저장하는 함수
-    def load_enabled_alarms(self):
+    async def load_enabled_alarms(self):
         # 데이터베이스의 condition 테이블에서 condition_id로 조회한 조건 정보를 Condition 객체로 리턴하는 함수
         # row_to_alarm 함수에서 조건 정보를 불러오기 위해 선언함
         def load_condition(condition_id: int) -> Condition:
@@ -121,46 +140,68 @@ class Watcher:
 
         column_list = ['alarm_id', 'channel_id', 'exchange_id', 'base_symbol', 'quote_symbol', 'condition_id']
 
-        # 업비트 알림을 불러옴
-        result_set = self.database.select(table_name='alarm', columns=column_list, exchange_id=UPBIT_ID, is_enabled=True)
-        self.enabled_upbit_alarm_list = [
-            row_to_alarm(alarm_dict) for alarm_dict in result_set.values()
-        ]
-
-        # 바이낸스 알림을 불러옴
-        result_set = self.database.select(table_name='alarm', columns=column_list, exchange_id=BINANCE_ID, is_enabled=True)
-        self.enabled_binance_alarm_list = [
-            row_to_alarm(alarm_dict) for alarm_dict in result_set.values()
-        ]
-
-
-    # 종목 데이터를 요청하고 cache 요소에 캐싱하는 함수
-    def cache_market_info(self, alarm: Alarm):
-        exchange = [self.upbit, self.binance][alarm.exchange_id - 1]
-        symbol = alarm.symbol
-
-        # 이미 해당 종목 데이터가 캐싱되어 있을 경우 함수를 종료
-        if symbol in self.cache[alarm.exchange_id]:
-            return
-
-        # 호가 정보를 저장하는 딕셔너리
-        # 딕셔너리의 구조는 다음 참조: https://docs.ccxt.com/#/?id=order-book
-        order_book = exchange.fetch_order_book(symbol)
-
-        # 체결 거래 정보 목록을 저장하는 리스트
-        # 리스트 구조는 다음 참조: https://docs.ccxt.com/#/?id=public-trades
-        current_timestamp = int(time.time() * 1000)
-        tick = exchange.fetch_trades(symbol=symbol, since=current_timestamp - self.CYCLE_MILLISECOND)
-        
-        # 일 단위 OHCLV 정보를 저장하는 리스트
-        # 리스트 구조는 다음 참조: https://docs.ccxt.com/#/?id=ohlcv-structure
-        ohclv = exchange.fetch_ohlcv(symbol=symbol, timeframe='1d')
-
-        self.cache[alarm.exchange_id][symbol] = {
-            'order_book': order_book,
-            'tick': tick,
-            'ohclv': {'1d': ohclv}
+        result_set = self.database.select(table_name='alarm', columns=column_list, is_enabled=True)
+        selected_alarm_dict = {
+            alarm_dict['alarm_id']: row_to_alarm(alarm_dict) for alarm_dict in result_set.values()
         }
+
+        # 데이터베이스에서 조회한 알람이 캐시되지 않은 새로운 알람일 경우 알람 캐시 딕셔너리에 추가
+        for alarm_id in selected_alarm_dict:
+            if alarm_id not in self.enabled_alarm_dict:
+                self.enabled_alarm_dict[alarm_id] = selected_alarm_dict[alarm_id]
+
+                # 필요한 과거의 캔들 데이터를 캐시
+                await self.cache_past_candle(selected_alarm_dict[alarm_id])
+
+        # 캐시된 알람이 데이터베이스에서 조회되지 않았을 경우 알람 캐시 딕셔너리에서 삭제
+        for alarm_id in self.enabled_alarm_dict.copy():
+            if alarm_id not in selected_alarm_dict:
+                self.enabled_alarm_dict.pop(alarm_id)
+
+
+    # 알람 조건을 최초로 감시하기 위해 필요한 과거의 캔들 데이터를 캐시하는 함수
+    async def cache_past_candle(self, alarm: Alarm):
+        exchange = [self.upbit, self.binance][alarm.exchange_id - 1]
+
+        fetched_candle_list_cache = []
+        
+        for condition_dict in [alarm.condition.bollinger_band, alarm.condition.rsi]:
+            # 해당 조건이 설정되어 있지 않으면 다음 조건으로 넘어감
+            if condition_dict == None:
+                continue
+
+            # 메모리 캐시에서 조건에 맞는 캔들 데이터를 조회
+            cached_candle_list = [
+                candle for candle in self.candle_cache
+                if candle.exchange_id == alarm.exchange_id 
+                and candle.symbol == alarm.symbol
+                and candle.interval == condition_dict['interval']
+            ]
+
+            # 이미 같은 조건의 캔들을 조회했다면 다음 조건으로 넘어감
+            if condition_dict['interval'] in fetched_candle_list_cache:
+                continue
+            
+            fetched_candle_list = exchange.fetch_ohlcv(symbol=alarm.symbol, timeframe=condition_dict['interval'], limit=100)
+            # 요청한 캔들 중 실시간 데이터인 마지막 캔들을 제외한 나머지 캔들을 돌면서 해당 캔들의 타임스탬프의 캔들이 캐시된 캔들인지 확인
+            for fetched_candle in fetched_candle_list[:-1]:
+                if [candle for candle in cached_candle_list if candle.timestamp * 1000 == fetched_candle[0]]:
+                    continue
+
+                # 해당 타임스탬프의 캔들이 캐시되어 있지 않은 경우 Candle 객체로 캐시
+                self.candle_cache.append(Candle(
+                    exchange_id=alarm.exchange_id,
+                    symbol=alarm.symbol,
+                    timestamp=int(fetched_candle[0] / 1000),    # 밀리초 단위의 타임스탬프를 초 단위로 환산하여 캐시
+                    interval=condition_dict['interval'],
+                    open=fetched_candle[1],
+                    highest=fetched_candle[2],
+                    lowest=fetched_candle[3],
+                    closing=fetched_candle[4],
+                    volume=fetched_candle[5]
+                ))
+
+            fetched_candle_list_cache.append(condition_dict['interval'])
 
 
     # 고래 호가를 찾는 함수
@@ -199,29 +240,28 @@ class Watcher:
         quantity = alarm.condition.tick['quantity']
         return [tick for tick in tick_list if tick['amount'] >= quantity]
 
-    
+
     # RSI 지표를 확인하는 함수
     # 리턴 값은 (조건 부합 여부, RSI 값, 과매수(True)/과매도(False) 여부)
     # 지정된 조건이 없을 경우 (True, None, None) 리턴
     # 조건에 부합하지 않은 경우 과매수/과매도 여부는 None 리턴
-    def check_rsi(self, alarm: Alarm) -> tuple:
+    async def check_rsi(self, alarm: Alarm, recent_trade_dict: dict) -> tuple:
         if alarm.condition.rsi == None:
             return (True, None, None)
 
         length = alarm.condition.rsi['length']
         interval = alarm.condition.rsi['interval']
 
-        # 해당 인터벌에 해당하는 OHCLV 데이터가 캐싱되어 있지 않은 경우 요청 후 캐싱
-        if interval not in self.cache[alarm.exchange_id][alarm.symbol]['ohclv']:
-            exchange = [self.upbit, self.binance][alarm.exchange_id - 1]
-            ohclv_list = exchange.fetch_ohlcv(symbol=alarm.symbol, timeframe=interval)
+        candle_cache = [
+            candle for candle in self.candle_cache
+            if candle.exchange_id == alarm.exchange_id
+            and candle.symbol == alarm.symbol
+            and candle.interval == interval
+        ][-99:]
 
-            self.cache[alarm.exchange_id][alarm.symbol]['ohclv'][interval] = ohclv_list
+        price_list = [candle.closing for candle in candle_cache].append(recent_trade_dict['price'])
 
-        ohclv_list = self.cache[alarm.exchange_id][alarm.symbol]['ohclv'][interval]
-        closing_price_list = [ohclv[4] for ohclv in ohclv_list]
-
-        rsi_value = functions.rsi(closing_price_list, length)
+        rsi_value = functions.rsi(price_list, length)
         max_value = alarm.condition.rsi['max_value']
         min_value = alarm.condition.rsi['min_value']
 
@@ -235,36 +275,29 @@ class Watcher:
     # 리턴 값은 (조건 부합 여부, 과매수(True)/과매도(False) 여부)
     # 지정된 조건이 없을 경우 (True, None) 리턴
     # 조건에 부합하지 않은 경우 과매수/과매도 여부는 None 리턴
-    def check_bollinger_band(self, alarm: Alarm) -> tuple:
+    def check_bollinger_band(self, alarm: Alarm, recent_trade_dict: dict) -> tuple:
         if alarm.condition.bollinger_band == None:
             return (True, None)
         
         length = alarm.condition.bollinger_band['length']
         interval = alarm.condition.bollinger_band['interval']
 
-        # 해당 인터벌에 해당하는 OHCLV 데이터가 캐싱되어 있지 않은 경우 요청 후 캐싱
-        if interval not in self.cache[alarm.exchange_id][alarm.symbol]['ohclv']:
-            exchange = [self.upbit, self.binance][alarm.exchange_id - 1]
-            ohclv_list = exchange.fetch_ohlcv(symbol=alarm.symbol, timeframe=interval)
+        candle_cache = [
+            candle for candle in self.candle_cache
+            if candle.exchange_id == alarm.exchange_id
+            and candle.symbol == alarm.symbol
+            and candle.interval == interval
+        ][-99:]
 
-            self.cache[alarm.exchange_id][alarm.symbol]['ohclv'][interval] = ohclv_list
-
-        # 길이만큼의 데이터만 불러옴
-        ohclv_list = self.cache[alarm.exchange_id][alarm.symbol]['ohclv'][interval][-length:]
-        closing_price_list = [ohclv[4] for ohclv in ohclv_list]
-
+        price_list = [candle.closing for candle in candle_cache].append(recent_trade_dict['price'])
         coefficient = alarm.condition.bollinger_band['coefficient']
-        basis_band, upper_band, lower_band = functions.bollinger_band(closing_price_list, coefficient)
+        basis_band, upper_band, lower_band = functions.bollinger_band(price_list, coefficient)
 
-        current_price = closing_price_list[-1]
+        current_price = price_list[-1]
         has_met_condition = lower_band >= current_price or upper_band <= current_price
         is_overbought = upper_band <= current_price if has_met_condition else None
 
         return (has_met_condition, is_overbought)
-
-
-    def clear_cache(self):
-        self.cache = {1: {}, 2: {}}
 
 
     def save_to_order_book_before(self):
@@ -273,25 +306,19 @@ class Watcher:
                 self.order_book_before[exchange_id][symbol] = self.cache[exchange_id][symbol]['order_book']
 
 
-    def alarm_check_cycle(self):
-        self.load_enabled_alarms()
+    async def check_alarm(self, alarm: Alarm):
+        exchange = [self.upbit, self.binance][alarm.exchange_id - 1]
+        while True:
+            trade = (await exchange.watch_trades(alarm.symbol))[0]
 
-        for alarm_list in [self.enabled_upbit_alarm_list, self.enabled_binance_alarm_list]:
             for alarm in alarm_list:
-                start = time.time()
-                self.cache_market_info(alarm)
-                print(f'Market info loaded: {time.time() - start}')
-
-                whale_dict = self.find_whale(alarm)
-                tick_list = self.check_tick(alarm)
-                bollinger_band_info = self.check_bollinger_band(alarm)
-                rsi_info = self.check_rsi(alarm)
+                # whale_dict = self.find_whale(alarm)
+                # tick_list = self.check_tick(alarm)
+                bollinger_band_info = self.check_bollinger_band(alarm, trade)
+                rsi_info = self.check_rsi(alarm, trade)
 
                 # 테스트용 코드
-                print('whale: ', whale_dict)
-                print('tick: ', tick_list)
+                # print('whale: ', whale_dict)
+                # print('tick: ', tick_list)
                 print('bollinger_band: ', bollinger_band_info)
                 print('rsi: ', rsi_info)
-
-        self.save_to_order_book_before()
-        self.clear_cache()
