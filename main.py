cf46ffaab2a3bc505c2728ede66437972974954f
angmusae2015 <angmusae2015@gmail.com>
2024-02-01 00:23:55 +0900

    HOTFIX

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import json
 
 from telebot.async_telebot import AsyncTeleBot
@@ -7,6 +8,8 @@ from watcher.watcher import Watcher
 
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(10 ** 7)
+
     with open('token.json', 'r') as file:
         tokens = json.load(file)
         database = Database(tokens['database_url'])
